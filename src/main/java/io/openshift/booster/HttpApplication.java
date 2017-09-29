b080da5e827c93eb4e87529f574ea77fb809a421
pbergeneOsiotest1 <pbergene+osiotest1@redhat.com>
2017-09-29 11:40:03 +0000

    Is it me you're looking for

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Hello, %s!";
+  protected static final String template = "Hello, %s!, is it me you're looking for";
 
   @Override
   public void start(Future<Void> future) {
